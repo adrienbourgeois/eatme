40b48ebbb75dd8bffb9faf84e29455e46020fcf9
Adrian <adrienfrancis.bourgeois@gmail.com>
2013-11-17 00:04:19 +1100

    setting up bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Showmeurfood::Application.routes.draw do
+
+  get 'pages/home'
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +43,7 @@ Showmeurfood::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
