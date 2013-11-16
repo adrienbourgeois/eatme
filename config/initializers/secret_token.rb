a7eee8412efa5714edbc51de17957300982c26b0
Adrian <adrienfrancis.bourgeois@gmail.com>
2013-11-16 23:57:32 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Showmeurfood::Application.config.secret_key_base = '881b3334e472ac008172e8e4c4ead8b31a3668c7521154026804e32e3ea32e78853b42c4e4f2a6841a770d0aed78950db73c6393dbfa489299d7d23d3c156fe5'
