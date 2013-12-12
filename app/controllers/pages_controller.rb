30aa0521e59436c753b26d85787ccb0ef14baf46
Adrian <adrienfrancis.bourgeois@gmail.com>
2013-12-12 12:54:47 +1100

    typo correction in the pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @number_of_photos = Photo.all.where(checked:true).count
+    @number_of_photos = Photo.where(checked:true).count
   end
 
 end
