79e7152a1aa3dfb1ae3f5652ee52b03ad3262602
Adrian <adrienfrancis.bourgeois@gmail.com>
2013-12-15 20:03:05 +1100

    update specs

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe Photo do
 
-  describe "latest" do
-    it "should return the last photo" do
-      last_photo = Photo.latest(1,1)[0]
-      last_photo.instagram_id.should eq 11
-    end
-  end
-
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -6,20 +6,21 @@ describe Place do
 
   describe "close" do
     it "should find close places when there is" do
-      close_places = Place.close(-33.0,181.0,3.0,1,10)
+      close_places = Place.close(-33.0,181.0,"3.0")
       close_places[0].name.should eq "Paramount Coffee Project"
     end
     it "should not find close places when there is not" do
-      close_places = Place.close(-33.0,182.0,3.0,1,10)
+      close_places = Place.close(-33.0,182.0,"3.0")
       close_places.count.should eq 0
     end
     it "should raise an exception if the radius is not authorized" do
-      lambda { Place.close(-33.0,182.0,-1.0,1,10) }.should raise_error(ArgumentError)
+      lambda { Place.close(-33.0,182.0,"-1.0") }.should raise_error(ArgumentError)
     end
   end
 
   describe "popular" do
     it "should order places by popularity" do
+      Information.create(name:'popular_places')
       Showmeurfood::Application.load_tasks
       Rake::Task['update_popular_places'].invoke
       popular_places = Place.popular
