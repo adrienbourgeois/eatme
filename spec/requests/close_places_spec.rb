14278046758d8925901c26cbd205db3bfd4d8f59
Adrian <adrienfrancis.bourgeois@gmail.com>
2014-01-06 20:16:39 +1100

    specs writing for reviews

--- spec/requests/close_places_spec.rb
+++ spec/requests/close_places_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 require 'pp'
 
-describe "JqtouchInterface", js: true  do
-
-  before do
-    I18n.enforce_available_locales = false
-  end
+describe "Close Places", js: true  do
 
   it "should be close_places as home page" do
     visit root_path
@@ -34,15 +30,6 @@ describe "JqtouchInterface", js: true  do
     select "0.1", from: 'distance'
     wait_for_ajax
     page.should have_content("No results found")
-  end
-
-  it "should display last recorded photo in 'just eaten' section" do
-    visit root_path
-    find("a", text: 'Menu').click
-    find("a", text: 'Just Eaten').click
-    wait_for_ajax
-    page.should have_content("Adriatic")
-    page.should have_content("Paramount Coffee Project")
   end
 
 end
