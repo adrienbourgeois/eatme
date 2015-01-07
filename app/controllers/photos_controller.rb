cecf83c13e9ec0f48957358e4bf892bb8661a685
Adrien Bourgeois <adrienfrancis.bourgeois@gmail.com>
2015-01-07 13:14:15 +1100

    update req to get the last pictures

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
   PER_PAGE = 9
 
   def index
-    photos = Photo.page(params[:page]).per(PER_PAGE).checked
+    photos = Photo.checked.where('created_at > (?)',24.hours.ago).page(params[:page]).per(PER_PAGE).order(created_at: :desc)
     photos_js = JSON.parse(photos.to_json(include: :place))
     photos_js.each { |photo| photo['minutes_ago'] = "#{time_ago_in_words(photo['updated_at'])} ago" }
     render json: photos_js
